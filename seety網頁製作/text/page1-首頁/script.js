4ed0e407c61d0bb015b75e6aecc55e3842352d95
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2022-12-21 16:02:27 +0800

    2022-12-21-V1-休息時間 沒進度

--- seety網頁製作/text/page1-首頁/script.js
+++ seety網頁製作/text/page1-首頁/script.js
@@ -1,16 +1,31 @@
 const listBar = document.querySelector('nav .more');
 const list = document.querySelector('nav .list')
 const place = document.querySelector('#living-room .big-room')
-const rooms =document.querySelector('.rooms');
+const rooms = document.querySelector('.rooms');
 
 
 
-listBar.addEventListener('click',()=>{
+listBar.addEventListener('click', () => {
         list.classList.toggle('show');
 
 })
 
-rooms.addEventListener('click',()=>{
+rooms.addEventListener('click', () => {
         place.classList.toggle('show');
         rooms.classList.toggle('mblong');
+})
+
+let observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+                entry.isIntersecting ?
+                        entry.target.classList.add('title-fade-in') : ''
+        })
+}, {
+        threshold: 0,
+        rootMargin:'0px 0px -200px 0px',
+})
+
+const titles = document.querySelectorAll('.section-title')
+titles.forEach(title=>{
+        observer.observe(title) ;
 })
