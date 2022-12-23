5b94541d2fa753a1608a3694cc987979c07a4bdf
lantium1994 <108165650+lantium1994@users.noreply.github.com>
2022-12-23 18:49:00 +0800

    1223
    back-to-top按鈕動畫
    prettier套件  自動排版了ＱＱ

--- seety網頁製作/text/page1-首頁/script.js
+++ seety網頁製作/text/page1-首頁/script.js
@@ -1,34 +1,45 @@
 const listBar = document.querySelector('nav .more');
-const list = document.querySelector('nav .list')
-const place = document.querySelector('#living-room .big-room')
+const list = document.querySelector('nav .list');
+const place = document.querySelector('#living-room .big-room');
 const rooms = document.querySelector('.rooms');
-
-
+const saturn = document.querySelector('.back-to-top');
+const banner = document.querySelector('.banner');
 
 listBar.addEventListener('click', () => {
-        list.classList.toggle('show');
-
-})
+  list.classList.toggle('show');
+});
 
 rooms.addEventListener('click', () => {
-        place.classList.toggle('show');
-        rooms.classList.toggle('mblong');
-})
+  place.classList.toggle('show');
+  rooms.classList.toggle('mblong');
+});
 
-let observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-                entry.isIntersecting ?
-                        entry.target.classList.add('title-fade-in') : ''
-        })
-}, {
-        threshold: 0,
-        rootMargin: '0px 0px -200px 0px',
-})
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.isIntersecting ? entry.target.classList.add('title-fade-in') : '';
+    });
+  },
+  {
+    threshold: 0,
+    rootMargin: '0px 0px -200px 0px',
+  }
+);
 
-const titles = document.querySelectorAll('.section-title')
-titles.forEach(title => {
-        observer.observe(title);
-})
+const titles = document.querySelectorAll('.section-title');
+titles.forEach((title) => {
+  observer.observe(title);
+});
+
+window.addEventListener('scroll', () => {
+  let scrollY = parseInt(window.scrollY);
+  let bannerHeight = parseInt(banner.offsetHeight);
+  if (scrollY >= bannerHeight) {
+    saturn.classList.add('appear');
+  } else {
+    saturn.classList.remove('appear');
+  }
+});
 
 // const paths = document.querySelectorAll('#svgPic path')
 
@@ -38,7 +49,7 @@ titles.forEach(title => {
 //         paths[i].style.strokeDashoffset = pL;
 
 //         setTimeout(() => {
-//                 paths[i].style.transition = `stroke-dashoffset 1.5s linear ${1 * i}s , 
+//                 paths[i].style.transition = `stroke-dashoffset 1.5s linear ${1 * i}s ,
 //         fill .5s linear ${1 * i}s
 //         `
 //                 paths[i].style.strokeDashoffset = pL + pL;
