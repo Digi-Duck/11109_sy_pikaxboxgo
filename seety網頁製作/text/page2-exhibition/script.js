ca28e899a54c66abbfb5d21f0b98a7d9774ea322
lantium1994 <108165650+lantium1994@users.noreply.github.com>
2022-12-27 19:30:18 +0800

    p2顯示
    p2 foreach innerhtml

--- seety網頁製作/text/page2-exhibition/script.js
+++ seety網頁製作/text/page2-exhibition/script.js
@@ -34,3 +34,34 @@ profPic.addEventListener('click', () => {
 optList.addEventListener('click', () => {
   navList.classList.toggle('switch');
 });
+
+const dataObj = async function () {
+  const response = await fetch('./dev-data/data.json').then();
+  const datas = await response.json();
+  console.log(datas);
+  renderContent = '';
+
+  datas.forEach((data) => {
+    console.log(data);
+    let renderContent = document.querySelector('.render-zone');
+    // renderContent.innerHTML = '';
+    renderContent.innerHTML += `
+    <figure class="information-card">
+          <div class="container">
+            <div class="image-area"><img src="${data.image}" alt="圖"></div>
+            <div class="content-area">
+              <h1>${data.exhibitionName}</h1>
+              <h4>表演者 : ${data.performer}</h4>
+              <p>${data.date.begin
+                .replace(',', '年')
+                .replace(',', '月')
+                .concat('日')}</p>
+              <a href="">詳細資訊</a>
+            </div>
+          </div>
+        </figure>
+    `;
+  });
+};
+
+dataObj();
