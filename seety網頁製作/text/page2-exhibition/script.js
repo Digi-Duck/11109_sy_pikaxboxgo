7d3a9cdd90c12d3e08e94c487e15d42284949531
lantium1994 <108165650+lantium1994@users.noreply.github.com>
2022-12-27 22:21:59 +0800

    p2 nav 統一
    nav風格統一

--- seety網頁製作/text/page2-exhibition/script.js
+++ seety網頁製作/text/page2-exhibition/script.js
@@ -1,38 +1,21 @@
-const profPic = document.querySelector('.prof-pic');
-const nav = document.querySelector('nav');
-const navBar = document.querySelector('.nav-bar');
-const optList = document.querySelector('.opt-list');
-const navList = document.querySelector('.nav-list');
+// const profPic = document.querySelector('.prof-pic');
+// const nav = document.querySelector('nav');
+// const navBar = document.querySelector('.nav-bar');
+// const optList = document.querySelector('.opt-list');
+// const navList = document.querySelector('.nav-list');
 const banner = document.querySelector('.banner');
+const saturn = document.querySelector('.back-to-top');
 
 // functions
 window.addEventListener('scroll', () => {
   let scrollHeight = parseInt(window.scrollY);
   let bannerHeight = parseInt(banner.offsetHeight);
   console.log(scrollHeight);
-  if (scrollHeight >= bannerHeight - 30) {
-    navBar.classList.add('just-center');
-    profPic.classList.add('opc');
+  if (scrollHeight >= bannerHeight) {
+    saturn.classList.add('appear');
   } else {
-    navBar.classList.remove('just-center');
-    profPic.classList.remove('opc');
+    saturn.classList.remove('appear');
   }
-  if (scrollHeight > bannerHeight + 25) {
-    nav.classList.add('top');
-  } else if (scrollHeight < bannerHeight - 25) {
-    nav.classList.remove('top');
-  }
-  if (scrollHeight > bannerHeight - parseInt(navList.offsetHeight)) {
-    navList.classList.remove('switch');
-  }
-});
-
-profPic.addEventListener('click', () => {
-  window.scrollTo(0, 0);
-});
-
-optList.addEventListener('click', () => {
-  navList.classList.toggle('switch');
 });
 
 const dataObj = async function () {
