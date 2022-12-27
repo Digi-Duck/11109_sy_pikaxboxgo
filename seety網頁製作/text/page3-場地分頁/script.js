5e76e24d7d80f93ecfe7e47e1a22e149fc11f019
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2022-12-27 16:06:46 +0800

    2022-12-27-V6
    首頁完成
    樂器分頁RWD ing

--- seety網頁製作/text/page3-場地分頁/script.js
+++ seety網頁製作/text/page3-場地分頁/script.js
@@ -1,1 +1,9 @@
+const listBar = document.querySelector('nav .more');
+const list = document.querySelector('nav .list');
 
+
+
+
+listBar.addEventListener('click', () => {
+    list.classList.toggle('show');
+  });
