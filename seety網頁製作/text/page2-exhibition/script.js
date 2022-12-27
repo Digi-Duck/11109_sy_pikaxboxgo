ac9d626eb53bfc5258d5c144fc0b144945595cf7
lantium1994 <108165650+lantium1994@users.noreply.github.com>
2022-12-28 00:00:07 +0800

    後端好煩
    學不下去跑來玩前端

--- seety網頁製作/text/page2-exhibition/script.js
+++ seety網頁製作/text/page2-exhibition/script.js
@@ -1,22 +1,26 @@
-// const profPic = document.querySelector('.prof-pic');
-// const nav = document.querySelector('nav');
-// const navBar = document.querySelector('.nav-bar');
-// const optList = document.querySelector('.opt-list');
-// const navList = document.querySelector('.nav-list');
 const banner = document.querySelector('.banner');
 const saturn = document.querySelector('.back-to-top');
+const blockAll = document.querySelector('.block-all');
 
 // functions
 window.addEventListener('scroll', () => {
   let scrollHeight = parseInt(window.scrollY);
   let bannerHeight = parseInt(banner.offsetHeight);
-  console.log(scrollHeight);
+  // console.log(scrollHeight);
   if (scrollHeight >= bannerHeight) {
     saturn.classList.add('appear');
   } else {
     saturn.classList.remove('appear');
   }
 });
+
+blockAll.addEventListener('click', () => {
+  if (blockAll.classList.contains('show')) {
+    blockAll.classList.remove('show');
+  }
+});
+
+let showArr = [];
 
 const dataObj = async function () {
   const response = await fetch('./dev-data/data.json').then();
@@ -25,26 +29,40 @@ const dataObj = async function () {
   renderContent = '';
 
   datas.forEach((data) => {
-    console.log(data);
+    // console.log(data);
     let renderContent = document.querySelector('.render-zone');
-    // renderContent.innerHTML = '';
-    renderContent.innerHTML += `
-    <figure class="information-card">
-          <div class="container">
-            <div class="image-area"><img src="${data.image}" alt="圖"></div>
+    showArr.push(`
+    <div class="image-area"><img src="${data.image}" alt="圖"></div>
             <div class="content-area">
               <h1>${data.exhibitionName}</h1>
               <h4>表演者 : ${data.performer}</h4>
+              <p>${data.performerDescription}</p>
               <p>${data.date.begin
                 .replace(',', '年')
                 .replace(',', '月')
                 .concat('日')}</p>
-              <a href="">詳細資訊</a>
+              <p>${data.description}</p>
             </div>
-          </div>
-        </figure>
-    `;
+    `);
+    renderContent.innerHTML += `<figure class="information-card"><div class="container"><div class="image-area"><img src="${
+      data.image
+    }" alt="圖"></div><div class="content-area"><h1>${
+      data.exhibitionName
+    }</h1><h4>表演者 : ${data.performer}</h4><p>${data.date.begin
+      .replace(',', '年')
+      .replace(',', '月')
+      .concat('日')}</p><button data-id="${
+      data.id
+    }"class="alink">詳細資訊</button></div></div></figure>`;
   });
+  const alink = document.querySelectorAll('.alink');
+  alink.forEach((link) => {
+    link.addEventListener('click', () => {
+      blockAll.classList.add('show');
+      blockAll.innerHTML = `${showArr[link.dataset.id]}`;
+    });
+  });
+  console.log(showArr);
 };
 
 dataObj();
