e4420941da271effdc229839b46789bc1da2e9c4
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2022-12-28 15:34:16 +0800

    2022-12-28-V2
    全完成 第一版

--- seety網頁製作/text/page1-首頁/script.js
+++ seety網頁製作/text/page1-首頁/script.js
@@ -3,7 +3,8 @@ const list = document.querySelector('nav .list');
 const place = document.querySelector('#living-room .big-room');
 const rooms = document.querySelector('.rooms');
 const roomImg = document.querySelectorAll('.roomImg');
-const saturn = document.querySelector('.back-to-top');
+const saturn = document.querySelector('.back-to-top-effectBox');
+const saturnObj = document.querySelector('.back-to-top');
 const banner = document.querySelector('.banner');
 const roomArticle = document.querySelectorAll('.room article');
 const openBtn = document.querySelector('.openBtn');
@@ -56,38 +57,57 @@ window.addEventListener('scroll', () => {
   }
 });
 
+saturn.addEventListener('mousemove', e => {
+  let rotateDeg = e.clientX - saturn.offsetLeft - saturnObj.offsetLeft;
+  if (rotateDeg < 0) {
+    saturnObj.style = `transform: translateX(-50%) rotate(${-rotateDeg / 4 - 45}deg);`
+  } else {
+    saturnObj.style = `transform: translateX(-50%) rotate(${-rotateDeg / 4 + 45}deg);`
+  }
+})
+
+saturn.addEventListener('mouseleave', () => {
+  saturnObj.style = `transform: translateX(-50%) rotate(0deg);`
+})
+
+
+
+
+
+
+
 const paths = document.querySelectorAll('#svgPic path')
 
 for (let i = 0; i < paths.length; i++) {
-        let pL = paths[i].getTotalLength();
-        paths[i].style.strokeDasharray = pL;
-        paths[i].style.strokeDashoffset = pL;
+  let pL = paths[i].getTotalLength();
+  paths[i].style.strokeDasharray = pL;
+  paths[i].style.strokeDashoffset = pL;
 
-        setTimeout(() => {
-                paths[i].style.transition = `stroke-dashoffset 1.5s linear ${1 * i}s ,
+  setTimeout(() => {
+    paths[i].style.transition = `stroke-dashoffset 1.5s linear ${1 * i}s ,
         fill .5s linear ${1 * i}s
         `
-                paths[i].style.strokeDashoffset = pL + pL;
-                setTimeout(() => {
-                        paths[i].setAttribute('fill', '#000')
-                }, 1500);
-        }, 100);
+    paths[i].style.strokeDashoffset = pL + pL;
+    setTimeout(() => {
+      paths[i].setAttribute('fill', '#000')
+    }, 1500);
+  }, 100);
 }
 
 const html = document.querySelector('html')
 const loadingPage = document.querySelector('#svgPic')
 window.onload = () => {
-        setTimeout(() => {
-                loadingPage.classList.add('unshow')
-                window.scrollTo({
-                        top:"0",
-                        behavior:"auto",
-                });
-                html.style.scrollBehavior = "smooth"
-                setTimeout(() => {
-                        loadingPage.style.display = "none";
-                }, 3000);
-        }, 8000);
+  setTimeout(() => {
+    loadingPage.classList.add('unshow')
+    window.scrollTo({
+      top: "0",
+      behavior: "auto",
+    });
+    html.style.scrollBehavior = "smooth"
+    setTimeout(() => {
+      loadingPage.style.display = "none";
+    }, 3000);
+  }, 8000);
 
 }
 
@@ -95,8 +115,8 @@ window.onload = () => {
 
 const navListTag = document.querySelectorAll('nav .list a');
 
-navListTag.forEach(tag =>{
-  tag.addEventListener('click',()=>{
+navListTag.forEach(tag => {
+  tag.addEventListener('click', () => {
     console.log(1);
     list.classList.remove('show')
   })
